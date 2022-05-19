846dd7a93897bca5c9855897c0b1e2ee07dea2d6
Aleks <aleks.tsybulenko@gmail.com>
2022-05-20 01:38:04 +0300

    1s homework

---
+++ javaLessons/src/main/java/school/lesson1/HomeWorkApp.java
@@ -0,0 +1,48 @@
+package school.lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange \nBanana \nApple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5, b = 1, c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 0;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 0, b = 1;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        if (a < b) {
+            System.out.println("a < b");
+        }
+    }
+
+}
+
